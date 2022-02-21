a41b7b495a1297aaecc99fcfb4f464ab8a972e7e
Anderson Igor <andersonigorf@gmail.com>
2022-02-20 23:47:38 -0300

    feat: db adapter

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"database/sql"
+
+	db2 "github.com/andersonigorf/go-hexagonal/adapters/db"
+	"github.com/andersonigorf/go-hexagonal/application"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func main() {
+
+	db, _ := sql.Open("sqlite3", "db.sqlite")
+
+	productDbAdapter := db2.NewProductDb(db)
+	productService := application.NewProductService(productDbAdapter)
+	product, _ := productService.Create("Product Example", 30)
+
+	productService.Enable(product)
+}
