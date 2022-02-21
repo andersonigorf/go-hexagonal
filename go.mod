43d7a6c8f4e9d071fcb1ab0615a3109b1cd798aa
Anderson Igor <andersonigorf@gmail.com>
2022-02-21 01:26:44 -0300

    feat: update cli adapter

--- go.mod
+++ go.mod
@@ -4,10 +4,9 @@ go 1.16
 
 require (
 	github.com/asaskevich/govalidator v0.0.0-20210307081110-f21760c49a8d
-	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/golang/mock v1.6.0
 	github.com/mattn/go-sqlite3 v1.14.11
 	github.com/satori/go.uuid v1.2.0
+	github.com/spf13/cobra v1.3.0
 	github.com/stretchr/testify v1.7.0
-	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
--- main.go
+++ main.go
@@ -1,21 +1,11 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
+*/
 package main
 
-import (
-	"database/sql"
-
-	db2 "github.com/andersonigorf/go-hexagonal/adapters/db"
-	"github.com/andersonigorf/go-hexagonal/application"
-
-	_ "github.com/mattn/go-sqlite3"
-)
+import "github.com/andersonigorf/go-hexagonal/cmd"
 
 func main() {
-
-	db, _ := sql.Open("sqlite3", "db.sqlite")
-
-	productDbAdapter := db2.NewProductDb(db)
-	productService := application.NewProductService(productDbAdapter)
-	product, _ := productService.Create("Product Example", 30)
-
-	productService.Enable(product)
+	cmd.Execute()
 }
